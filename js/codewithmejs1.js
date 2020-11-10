a046989df6394d832d2b6a34e8ff631a2b994634
Swatilekha Roy <swatilekha.roy4@gmail.com>
2020-11-10 20:25:49 +0530

    Create codewithmejs1.js

---
+++ js/codewithmejs1.js
@@ -0,0 +1,13 @@
+//This code belongs to Colleen van Lent.
+function openMe()
+{
+   x=document.getElementById("demo");
+   x.style.display="block";
+}
+
+function closeMe()
+{
+   x=document.getElementById("demo");
+   //x.className="closed";
+   x.style.display="none";
+}

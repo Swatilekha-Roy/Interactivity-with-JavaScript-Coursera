e5b6fcbfd1ee71a83fd747d8034cf53ef14fa1bb
Swatilekha Roy <swatilekha.roy4@gmail.com>
2020-11-12 20:52:11 +0530

    Add files via upload

---
+++ js/Autocomplete With JS.js
@@ -0,0 +1,20 @@
+function billingFunction()
+{
+	 var shippingname=document.getElementById('shippingName');
+	 var shippingzip=document.getElementById('shippingZip');
+	 var billingname=document.getElementById('billingName');
+	 var billingzip=document.getElementById('billingZip');
+
+	 if(document.getElementById('same').checked)
+	 {
+	    billingname.value=shippingname.value;
+	    billingzip.value=shippingzip.value;
+	    billingname.setAttribute("required",true);
+	    billingzip.setAttribute("required",true);
+	 }
+	 else
+	 {
+	    billingname.removeAttribute("required");
+	    billingzip.removeAttribute("required");
+	 }
+}
